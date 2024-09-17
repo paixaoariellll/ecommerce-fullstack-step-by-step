6e0321816d96daddf45d085b1f9666fb0321ba70
Ariel Paixão <paixaoariellll@gmail.com>
2024-09-17 12:23:44 -0300

    feat: init project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ecommerce-fullstack-step-by-step
+
 Esse projeto é apenas para ensinar a utilizar e configurar todos os passos de um ecommerce completo desde o início do projeto
+
+# Set Up do projeto Inicial
+
+## Criando um novo projeto
+
+## Adicionando a biblioteca Material
+
+## Adicionando a biblioteca Tailwind CSS
---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatButtonModule, MatCardModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'ecommerce';
+}
